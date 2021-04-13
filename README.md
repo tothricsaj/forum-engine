9536468c215425b64aff93118e14f49be1fbf05d
tothricsaj <ricktothdevelop@gmail.com>
2021-04-13 16:26:43 +0200

    added init query to create tables

---
+++ README.md
@@ -0,0 +1,7 @@
+# Forum engine
+
+### user storys
+* I want to create a profile
+* I can create topics
+* I can create comment
+* I can like comment
--- app.js
+++ app.js
@@ -7,6 +7,56 @@ const app = express()
 
 const port = 3007
 
+const firstDownload = true
+
+const createTables = (req, res, next) => {
+
+  const initQuery = `
+    BEGIN;
+    CREATE TABLE IF NOT EXISTS user_profile (
+      user_id    serial PRIMARY KEY,
+      user_name  varchar(70) NOT NULL,
+      email      varchar(255) NOT NULL
+    );
+
+    CREATE TABLE IF NOT EXISTS topic (
+      topic_id   serial PRIMARY KEY ,
+      topic_name varchar(70) NOT NULL
+    );
+    COMMIT;
+  `
+
+  // const initQuery = `
+  //   CREATE TABLE IF NOT EXISTS user_profile (
+  //     user_id    serial PRIMARY KEY,
+  //     user_name  varchar(70) NOT NULL,
+  //     email      varchar(255) NOT NULL
+  //   );
+  // `
+      // adsfasdf
+  const testQuery = `
+    CREATE TABLE IF NOT EXISTS accounts (
+      user_id serial PRIMARY KEY,
+      username VARCHAR ( 50 ) UNIQUE NOT NULL,
+      password VARCHAR ( 50 ) NOT NULL,
+      email VARCHAR ( 255 ) UNIQUE NOT NULL
+    );
+  `
+  if(firstDownload) {
+    db
+      .query(initQuery)
+      // .query(testQuery)
+      .then(dbRes => {
+        console.log(dbRes)
+        firstDownload = false
+        next()
+      })
+      .catch(err => console.log(err))
+  }
+
+  next()
+}
+
 app.set('view engine', 'ejs')
 app.set('views', 'view')
 
@@ -14,25 +64,14 @@ app.use(express.static('public'))
 
 app.use(bodyParser.urlencoded({extended: false}))
 
-const queryStr = `
-  SELECT
-    title, release_year
-  FROM
-    film
-  WHERE
-    length < $1
-  LIMIT $2
-`
+app.use(createTables)
+
+let counter = 0
 
 app.get('/', (req, res, next) => {
-  db
-    .query(queryStr, [100, 5])
-    .then(dbRes => {
-      res.render('home', {
-        films: dbRes.rows
-      })
-    })
-    .catch(err => console.log(err))
+  counter++
+  console.log(`home is called ${counter} times!`)
+  res.render('home')
 })
 
 app.listen(port)
